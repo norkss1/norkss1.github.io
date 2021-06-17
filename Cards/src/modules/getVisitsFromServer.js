83968a86a365a70bf1c742ca2faf14546a38cbe1
Roman Kalychak <home96.09@gmail.com>
2021-06-17 21:47:16 +0300

    Add project Cards

---
+++ Cards/src/modules/getVisitsFromServer.js
@@ -0,0 +1,39 @@
+import API from "./API.js";
+import {visitLayout} from '../filters/filters';
+import {arrayVisitsView} from '../scripts/index';
+import {VisitCardiologist, VisitDentist, VisitTherapist} from "../models/visitsModel";
+
+
+function getVisits() {
+
+    API.getCards()
+        .then(responses => {
+               responses.forEach(response => {
+           createVisitForView(response)
+
+        })
+            visitLayout(arrayVisitsView)
+})
+}
+
+function createVisitForView(object){           //создает обьект согласно нужного класса. Добавялет обьект в массив для отображения
+
+    let cardObjView = {}
+
+    if(object.doctor === 'Стоматолог'){
+        cardObjView = new VisitDentist(object)
+    }else
+    if (object.doctor === 'Кардиолог'){
+        cardObjView = new VisitCardiologist(object)
+    }else
+    if (object.doctor === 'Терапевт'){
+        cardObjView = new VisitTherapist(object)
+    }
+
+    arrayVisitsView.push(cardObjView)
+
+}
+
+
+export {getVisits}
+export {createVisitForView}
